db8ec0439ce12c334d56c57f6f4b61841e490ac5
Andrew Galloway <andrew@cookbrite.com>
2016-01-19 22:20:15 -0800

    adds delete function

--- recursive_file_management.py
+++ recursive_file_management.py
@@ -3,11 +3,26 @@ import argparse
 
 
 def rename_files(directory_to_crawl, change_from, change_to):
+    count = 0
     for root, dirs, files in os.walk(directory_to_crawl):
         for f in files:
             if f == change_from:
                 os.rename(os.path.join(root, change_from), os.path.join(root, change_to))
                 print('{} renamed to {}'.format(change_from, change_to))
+                count += 1
+    print('{} files modified'.format(count))
+
+
+def delete_files(directory_to_crawl, filename_to_delete):
+    count = 0
+    for root, dirs, files in os.walk(directory_to_crawl):
+        for f in files:
+            if f == filename_to_delete:
+                target = os.path.join(root, filename_to_delete)
+                os.remove(target)
+                print('{} deleted'.format(target))
+                count += 1
+    print('{} files deleted'.format(count))
 
 
 def main():
@@ -20,20 +35,33 @@ def main():
         help='The directory from which to crawl'
     )
     parser.add_argument(
-        'current_file_name',
+        'target_file_name',
         type=str,
-        help='The name of the file(s) you wish to rename'
+        help='The name of the file(s) on which you wish to operate'
     )
     parser.add_argument(
-        'intended_file_name',
+        '-r', '--rename',
         type=str,
         help='The name you wish to give your files'
     )
+    parser.add_argument(
+        '-d', '--delete',
+        action='store_true',
+        default=False,
+        help='Delete all occurrences of target_file_name in directory tree'
+    )
     args = parser.parse_args()
 
-    rename_files(args.directory,
-                 args.current_file_name,
-                 args.intended_file_name)
+    if args.rename and args.delete:
+        print('You make no sense')
+        exit()
+    elif args.rename:
+        rename_files(args.directory,
+                     args.target_file_name,
+                     args.rename)
+    elif args.delete:
+        delete_files(args.directory,
+                     args.target_file_name)
 
 
 if __name__ == '__main__':
