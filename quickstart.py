8a83669fed60d67eaa123edbf40f6ea711f373cd
Andrew <akq.galloway@lightsphereai.com>
2016-08-20 19:28:10 -0700

    changes api permission scope

--- quickstart.py
+++ quickstart.py
@@ -18,8 +18,8 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
+SCOPES = 'https://www.googleapis.com/auth/gmail.compose'
+CLIENT_SECRET_FILE = 'data/client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
 
 
