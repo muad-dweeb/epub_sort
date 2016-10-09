bb3217a17c24a72f4866fffaeda177613f4970c4
Andrew <akq.galloway@lightsphereai.com>
2016-10-09 01:04:08 -0700

    initial commit for idea

---
+++ product_code_lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Intent:
+    Given a product code, UPC, PLU, SKU, etc.,
+    search relevant sites/databases for reasonable
+    product matches. Return a string resembling
+    a best guess for product name.
+
+Sources:
+    - http://www.upcitemdb.com/
+    - https://www.buycott.com/
+
+
+"""
