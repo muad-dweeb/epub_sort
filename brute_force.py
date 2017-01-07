1d4c3407fdecc0a4133c2ca219533e53ee32f8d3
Andrew <akq.galloway@lightsphereai.com>
2017-01-07 00:54:17 -0800

    Improved encfs cli password cracker... sort of
    - there has to be a more efficient way to do this
    - at this rate of execution, I can expect to recover my encfs password some time in the next 150,000 years

--- brute_force.py
+++ brute_force.py
@@ -1,37 +1,57 @@
-#!/usr/bin/env python
+#!usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import itertools
-from random import choice
-from string import ascii_lowercase, ascii_letters, ascii_uppercase, printable
-from subprocess import call, check_call, check_output
 from argparse import ArgumentParser
-
-import pexpect
 from os import path
+from string import ascii_lowercase, ascii_letters, ascii_uppercase, printable
+
+import time
+
+from datetime import datetime
+from pexpect import spawn
+
+from utility_exceptions import UtilityException
 
 
 class Crack():
     def __init__(self):
         self.attempted_strings = []
+        self.password = ''
         # self.password_mode = mode
 
-    def brute_force_encfs(self, password_length, encrypted_dir, visible_dir):
-        char_set = ['n','b','m','h','j','u','i','y','j','k',';','l','o','a','d','s','c','v','g','f','r','e','w','t']
-        passwords_list = (itertools.product(char_set, repeat=password_length))
+    def brute_force_encfs(self, password_length, char_set, encrypted_dir, visible_dir):
+        if password_length:
+            passwords_list = (itertools.product(char_set, repeat=password_length))
+        else:
+            passwords_list = (itertools.product(char_set))
         for p in passwords_list:
             # log_file = 'brute_force.log'
             password = (''.join(p))
-            print(password)
-            # print(check_call('encfs', encrypted_dir, visible_dir))
-            child = pexpect.spawn('encfs {} {}'.format(encrypted_dir, visible_dir))
-            # child.logfile_read = open(log_file, 'ab')
-            child.expect(['EncFS Password: '])
-            child.sendline(password)
-            print(child)
+            if password not in self.attempted_strings:
+                print(password)
+                # print(check_call('encfs', encrypted_dir, visible_dir))
+                try:
+                    child = spawn('encfs {} {}'.format(encrypted_dir, visible_dir))
+                    # print(child.before)  # DEBUG
+                    # child.logfile_read = open(log_file, 'ab')
+                    child.expect('EncFS Password: ')
+                    # time.sleep(0.1)
+                    child.sendline(password)
+                    result = child.expect(['Error decoding volume key, password incorrect', '[#\S] '])
+                    if result == 0:
+                        # print('Killing child {}'.format(child.name))
+                        child.kill(0)
+                        self.attempted_strings += password
+                    elif result == 1:
+                        print('Password found: {}'.format(password))
 
+                except Exception as e:
+                    print(e.message)
 
-    #     # mode: alpha_lower, numeric, all, etc.
+    """
+    mode: alpha_lower, numeric, all, etc.
+    """
     # def random_password_generator(self, password_length, password_mode):
     #     if password_mode == 'alpha_lower':
     #         mode = ascii_lowercase
@@ -46,30 +66,57 @@ class Crack():
     #     self.attempted_strings.append(password)
     #     return password
 
-def permute(length):
-    return itertools.product(ascii_lowercase, repeat=length)
+
+# def permute(length):
+#     return itertools.product(ascii_lowercase, repeat=length)
 
 
 def parse_args():
     parser = ArgumentParser(description='Password Get')
-    parser.add_argument('--absolute_length', '-al', type=int, optional=True,
+    parser.add_argument('--absolute_length', '-al', type=int, default=None,
                         help='Known character length of the target password')
-    parser.add_argument('--encfs', type=tuple, default=False,
-                        help='A tuple of paths (strings) in the form: (encrypted_dir, visible_dir)')
+    parser.add_argument('--encfs', type=str, default=False,
+                        help='A string of paths delimited by space in the form: encrypted_dir visible_dir')
+    parser.add_argument('--char_set', '-c', type=str, default='printable',
+                        help='Set of characters from which to build passwords.')
     args = parser.parse_args()
     return args
+
+
+def select_character_set(string_char_set):
+    # ascii_lowercase, ascii_letters, ascii_uppercase, printable
+    if string_char_set.lower().replace('_', ' ') == 'ascii lowercase':
+        char_set = ascii_lowercase
+    elif string_char_set.lower().replace('_', ' ') == 'ascii letters':
+        char_set = ascii_letters
+    elif string_char_set.lower().replace('_', ' ') == 'ascii uppercase':
+        char_set = ascii_uppercase
+    elif string_char_set.lower() == 'printable':
+        char_set = printable
+    else:
+        char_set = string_char_set.split(' ')
+        # raise UtilityException('Invalid String character set specified.')
+    print('Character Set:\n\t{}'.format(char_set))
+    return char_set
 
 
 def main():
     args = parse_args()
     char_length = args.absolute_length
+    char_set = select_character_set(args.char_set)
 
+    start_time = datetime.now()
     recover_my_damn_password = Crack()
 
     if args.encfs:
-        encrypted_dir = path.expanduser(args.encfs[0])
-        visible_dir = path.expanduser(args.encfs[1])
-        recover_my_damn_password.brute_force_encfs(char_length, encrypted_dir, visible_dir)
+        paths = args.encfs.split(' ')
+        encrypted_dir = path.expanduser(paths[0])
+        visible_dir = path.expanduser(paths[1])
+        recover_my_damn_password.brute_force_encfs(char_length, char_set, encrypted_dir, visible_dir)
+
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print('Time elapsed: {}'.format(run_time))
 
 
 if __name__ == '__main__':
